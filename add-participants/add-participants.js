2edb94c499d7762047df80e6ca62298a1ec145b7
Brien Thomas <thomasbrien94@gmail.com>
2022-05-24 10:49:52 -0700

    onLoad function for add-participants.js correctly displays the workshop options to the select dropdown

--- add-participants/add-participants.js
+++ add-participants/add-participants.js
@@ -1,9 +1,14 @@
-import { addParticipant } from '../fetch-utils.js';
+import { addParticipant, getWorkshops } from '../fetch-utils.js';
+import { renderSelectOptions } from '../render-utils.js';
 
 const addParticipantForm = document.getElementById('participant-form');
-const selectWorkshop = document.getElementById('select-workshops');
+const workshopSelect = document.getElementById('select-workshops');
 
 async function onLoad() {
     const workshops = await getWorkshops();
-
+    for (let workshop of workshops) {
+        const workshopOption = renderSelectOptions(workshop);
+        workshopSelect.append(workshopOption);
+    }
 }
+onLoad();
